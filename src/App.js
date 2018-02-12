67ae65473676fdea9e5fc198487d27c9339c68e7
milliampere <millla@hotmail.com>
2018-02-12 13:31:58 +0100

    Search with Fuse.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { graphql } from 'react-apollo'
-import gql from 'graphql-tag'
-import NutritionCalculator from './components/NutritionCalculator';
+//import { graphql } from 'react-apollo'
+//import gql from 'graphql-tag'
 import Navigation from './components/Navigation'
-import Table from './components/Table';
+//import Table from './components/Table';
 import TextArea from './components/TextArea';
 import Credits from './components/Credits';
+//import { debug } from 'util';
+import allFoods from './nameList.json'
 
 class App extends Component {
 
@@ -14,16 +15,14 @@ class App extends Component {
     selectedFoodNames: []
   }
 
-
   render() {
 
-    const { loading, error, allFoods } = this.props.data;
-
+    //const { loading, error, allFoods } = this.props.data;
 
     return (
       <div className="App">
         <Navigation selectedType={this.state.selectedType} />
-        <Table selectedType={this.state.selectedType} selectedFoodNames={this.props.selectedFoodNames}/>
+        {/* <Table selectedType={this.state.selectedType} selectedFoodNames={this.props.selectedFoodNames}/> */}
         <TextArea selectedFoodNames={this.props.selectedFoodNames} allFoods={allFoods}/>
         <Credits />
       </div>
@@ -33,13 +32,14 @@ class App extends Component {
 
 
 //Get this list of foodnames and id:s when the plugin is activated (called when open)
-export const allFoods = gql`
+/* export const allFoods = gql`
   query allFoods {
     allFoods {
       id
       name
     }
   }
-`
+` */
 
-export default graphql(allFoods)(App)
+//export default graphql(allFoods)(App)
+export default App;
--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Fuse from 'fuse.js';
 
 class TextArea extends Component {
 
@@ -7,22 +8,70 @@ class TextArea extends Component {
         foodArray: []
     }
 
+
     handleChange = (event) => {
         this.setState({input: event.target.value});
         this.separateRows();
-    }
 
+        this.state.foodArray.map((row) => {
+            const name = this.identifyName(row);
+            this.search(name);
+            return row
+        })
+        
+    }
 
     separateRows = () => {
         const arrayOfFoods = this.state.input.split("\n");
-        console.log(arrayOfFoods);
         this.setState({foodArray: arrayOfFoods});
     }
 
+    identifyName = (string) => {
+
+        const units = [
+            "kilo", "kg", "gram", "g", "milligram", "mg", 
+            "liter", "l", "deciliter", "dl", "centiliter", "cl", "milliliter", "ml",
+            "matsked", "msk", "tesked", "tsk", "kryddmått", "krm", 
+            "blad", "krukor", "kruka", "koppar", "kopp", "nypor", "nypa", "stycken", "st", "förpackning", "förpackningar", "förp", "klyftor", "klyfta"
+        ];
+
+        const unit = units.filter(unit => {
+            return string.includes(" " + unit + " ");
+        })
+
+        if(unit.length > 0){
+            const array = string.split(unit[0]); 
+            return array[1];
+        }
+        else{
+            return string;
+        }
+    }
+
+    search = (name) => {
+
+        const list = this.props.allFoods;
+
+        var options = {
+            shouldSort: true,
+            tokenize: true,
+            matchAllTokens: true,
+            threshold: 0.4,
+            location: 0,
+            distance: 100,
+            maxPatternLength: 32,
+            minMatchCharLength: 1,
+            keys: [
+              "name"
+            ]
+        };
+
+        var fuse = new Fuse(list, options); // "list" is the item array
+        var result = fuse.search(name);
+        console.log(result);
+    }
 
     render() {
-
-        console.log(this.state.foodArray);
 
         return (
             <div>
