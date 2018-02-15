61c2b02fb40e2a87022197ce1f55fd08a45eb2fa
milliampere <millla@hotmail.com>
2018-02-15 15:50:16 +0100

    Now in a function.

--- src/utils/regex.js
+++ src/utils/regex.js
@@ -1,52 +1,57 @@
-let inputString = "1,5 msk vaniljpulver".trim();
+function useRegex(inputString = '') {
 
-const re1 = /(\d+)\s+(kilo|kg|gram|g|milligram|mg|liter|l|deciliter|dl|centiliter|cl|milliliter|ml|matsked|msk|tesked|tsk|kryddmått|krm|blad|krukor|kruka|koppar|kopp|nypor|nypa|stycken|st|förpackning|förpackningar|förp|klyftor|klyfta)\s(\D+)/;
-const re2 = /\d+\s+\D/;
-const re3 = /^\D+/;   // \D is "not digit"
+    inputString = inputString.trim();
 
-let ingredientArray = [];
-let ingredientObject = {
-    amount: 0,
-    type: '',
-    name: ''
-};
+    const re1 = /(\d+)\s+(kilo|kg|gram|g|milligram|mg|liter|l|deciliter|dl|centiliter|cl|milliliter|ml|matsked|msk|tesked|tsk|kryddmått|krm|blad|krukor|kruka|koppar|kopp|nypor|nypa|stycken|st|förpackning|förpackningar|förp|klyftor|klyfta)\s(\D+)/;
+    const re2 = /\d+\s+\D/;
+    const re3 = /^\D+/;   // \D is "not digit"
 
-if(inputString.match(re1)){
-    //separate amount, type and name 
-    ingredientArray = inputString.split(/\s(kilo|kg|gram|g|milligram|mg|liter|l|deciliter|dl|centiliter|cl|milliliter|ml|matsked|msk|tesked|tsk|kryddmått|krm|blad|krukor|kruka|koppar|kopp|nypor|nypa|stycken|st|förpackning|förpackningar|förp|klyftor|klyfta)\s/);
-    ingredientObject.amount = ingredientArray[0];
-    ingredientObject.type = ingredientArray[1];
-    ingredientObject.name = ingredientArray[2];
+    let ingredientArray = [];
+    let ingredientObject = {
+        amount: 0,
+        type: '',
+        name: ''
+    };
 
-} else if (inputString.match(re2)) {
-    //separate amount and name 
-    index = inputString.search(/\d\s+\D/);
-    ingredientObject.amount = inputString.substring(0,index+1);
-    ingredientObject.name = inputString.slice(index+2);
+    if(inputString.match(re1)){
+        //separate amount, type and name 
+        ingredientArray = inputString.split(/\s(kilo|kg|gram|g|milligram|mg|liter|l|deciliter|dl|centiliter|cl|milliliter|ml|matsked|msk|tesked|tsk|kryddmått|krm|blad|krukor|kruka|koppar|kopp|nypor|nypa|stycken|st|förpackning|förpackningar|förp|klyftor|klyfta)\s/);
+        ingredientObject.amount = ingredientArray[0];
+        ingredientObject.type = ingredientArray[1];
+        ingredientObject.name = ingredientArray[2];
 
-} else if (inputString.match(re3)) {
-    // don't separate
-    ingredientObject.name = inputString; 
+    } else if (inputString.match(re2)) {
+        //separate amount and name 
+        index = inputString.search(/\d\s+\D/);
+        ingredientObject.amount = inputString.substring(0,index+1);
+        ingredientObject.name = inputString.slice(index+2);
+
+    } else if (inputString.match(re3)) {
+        // don't separate
+        ingredientObject.name = inputString; 
+    }
+
+    for (let property in ingredientObject){
+        if(typeof ingredientObject[property] === 'string'){
+            // remove whitespace
+            ingredientObject[property] = ingredientObject[property].trim();
+
+            // change amount to number
+            if(property === 'amount'){
+                if(ingredientObject[property].match(/\d+/)){
+                    ingredientObject[property] = ingredientObject[property].replace(',','.');
+                    ingredientObject[property] = Number(ingredientObject[property])   
+                }  
+            }
+
+        } else if(typeof ingredientObject[property] === 'undefined') {
+            // reset if undefined
+            ingredientObject[property] = '';
+        }
+    }  
+
+    return ingredientObject;
+
 }
 
-for (let property in ingredientObject){
-    if(typeof ingredientObject[property] === 'string'){
-        // remove whitespace
-        ingredientObject[property] = ingredientObject[property].trim();
-
-        // change amount to number
-        if(property === 'amount'){
-            if(ingredientObject[property].match(/\d+/)){
-                ingredientObject[property] = ingredientObject[property].replace(',','.');
-                ingredientObject[property] = Number(ingredientObject[property])   
-            }  
-        }
-
-    } else if(typeof ingredientObject[property] === 'undefined') {
-        // reset if undefined
-        ingredientObject[property] = '';
-    }
-}  
-
-console.log(ingredientObject);
-
+console.log(useRegex("1,5 msk vaniljpulver"));
