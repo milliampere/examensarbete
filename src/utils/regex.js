bce35f36d9cb780e8c2c25afeee5ccbd953941f4
milliampere <millla@hotmail.com>
2018-02-15 12:48:30 +0100

    More units.

--- src/utils/regex.js
+++ src/utils/regex.js
@@ -1,23 +1,23 @@
 let inputString = "3 msk vaniljsocker".trim();
 
-const re1 = /(\d+)\s*(msk|tsk|g)\s*(\w+)/;
+const re1 = /(\d+)\s*(kilo|kg|gram|g|milligram|mg|liter|l|deciliter|dl|centiliter|cl|milliliter|ml|matsked|msk|tesked|tsk|kryddmått|krm|blad|krukor|kruka|koppar|kopp|nypor|nypa|stycken|st|förpackning|förpackningar|förp|klyftor|klyfta)\s*(\w+)/;
 const re2 = /\d+\s+\w+/;
 const re3 = /^\w+/;
 
 let ingredient = [];
 
 if(inputString.match(re1)){
-    //dela upp i siffra, mått, ingrediens 
-    ingredient = inputString.split(/(msk|tsk|g)/);
+    //separate amount, unit and ingredient 
+    ingredient = inputString.split(/\s(kilo|kg|gram|g|milligram|mg|liter|l|deciliter|dl|centiliter|cl|milliliter|ml|matsked|msk|tesked|tsk|kryddmått|krm|blad|krukor|kruka|koppar|kopp|nypor|nypa|stycken|st|förpackning|förpackningar|förp|klyftor|klyfta)\s/);
 
 } else if (inputString.match(re2)) {
-    //dela upp i siffra och ingrediens
+    //separate amount and ingredient 
     index = inputString.search(/\d\s+\w/);
     ingredient.push(inputString.substring(0,index+1));
     ingredient.push(inputString.slice(index+2));
 
 } else if (inputString.match(re3)) {
-    //dela inte upp, sök direkt på ingrediens
+    // don't separate
     ingredient.push(inputString); 
 }
 
