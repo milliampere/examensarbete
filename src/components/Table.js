b23fc061e950db5dfd189cbc69767c4e00634195
milliampere <millla@hotmail.com>
2018-02-27 11:15:00 +0100

    Nutritional values for minerals.

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,11 +3,15 @@ import React, { Component } from 'react';
 // import gql from 'graphql-tag'
 import TableRow from './TableRow';
 import Button from './Button';
-
+import fakeProps1 from '../data/nutritionForOneFood';
+import fakeProps2 from '../data/woman31-60';
 
 class Table extends Component {
 
 	render() {
+
+		//console.log(fakeProps1.data);
+		//console.log(fakeProps2.data);
 
 		const { handleChange, onFocus, foodArray, resultArray, activeIndex } = this.props;
 
@@ -23,8 +27,21 @@ class Table extends Component {
 				ingredients={foodArray}
 				result={resultArray[index]}
 				activeIndex={activeIndex}
+				nutritionValues={fakeProps1.data.Food.nutritions}
 			/>
 		})
+
+		const getRecommendedValue = (abbr) => {
+			const nutrition = fakeProps2.data.allNutrients.find((nutrient) => { return nutrient.abbreviation === abbr});
+			if(nutrition) { return nutrition.woman3160 } 
+			else { return 0 }
+		};
+
+		const getNutritionNameAndUnit = (abbr) => {
+			const nutrition = fakeProps2.data.allNutrients.find((nutrient) => { return nutrient.abbreviation === abbr});
+			if(nutrition) { return `${nutrition.name} (${nutrition.unitforRI}) ` } 
+			else { return '' }
+		};
 
 		return (
 			<div>
@@ -35,10 +52,34 @@ class Table extends Component {
 							<th>Mått</th>
 							<th>Ingredienser</th>
 							<th>Sökresultat</th>
+							<th>{getNutritionNameAndUnit('P')}</th>
+							<th>{getNutritionNameAndUnit('I')}</th>
+							<th>{getNutritionNameAndUnit('Fe')}</th>
+							<th>{getNutritionNameAndUnit('Ca')}</th>
+							<th>{getNutritionNameAndUnit('K')}</th>
+							<th>{getNutritionNameAndUnit('Cu')}</th>
+							<th>{getNutritionNameAndUnit('Mg')}</th>
+							<th>{getNutritionNameAndUnit('Se')}</th>
+							<th>{getNutritionNameAndUnit('Zn')}</th>
 						</tr>
 					</thead>
 					<tbody>
 						{foods}
+						<tr>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td>{getRecommendedValue('P')}</td>
+							<td>{getRecommendedValue('I')}</td>
+							<td>{getRecommendedValue('Fe')}</td>
+							<td>{getRecommendedValue('Ca')}</td>
+							<td>{getRecommendedValue('K')}</td>
+							<td>{getRecommendedValue('Cu')}</td>
+							<td>{getRecommendedValue('Mg')}</td>
+							<td>{getRecommendedValue('Se')}</td>
+							<td>{getRecommendedValue('Zn')}</td>
+						</tr>
 					</tbody>
 				</table>
 				<Button name="visa innehåll" handleClick={this.handleClick}/>
--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -2,9 +2,18 @@ import React from 'react';
 import './TableRow.css';
 import DropDownMenu from './DropDownMenu.js';
 
+
+
 const TableRow = (props) => {
 
-    const { ingredients, index, handleChange, result, onClick, onFocus, activeIndex, onBlur } = props;
+    const { ingredients, index, handleChange, result, onClick, onFocus, activeIndex, onBlur, nutritionValues, activeSetOfNutrients } = props;
+
+    const getNutritionalValue = (abbr) => {
+        const nutrition = nutritionValues.find((nutrient) => { return nutrient.abbreviation === abbr});
+    
+        if(nutrition) { return nutrition.value } 
+        else { return 0 }
+    };
 
     return (
         <tr>
@@ -12,6 +21,15 @@ const TableRow = (props) => {
             <td><input className='input-small' type='text' value={ingredients[index].type} onChange={(e) => handleChange(e, index, 'type')}></input></td>
             <td><DropDownMenu indexInput={index} name={ingredients[index].name} ingredients={ingredients} result={result} onChange={handleChange} onClick={onClick} onFocus={onFocus} activeIndex={activeIndex} onBlur={onBlur}/></td>
             <td className='input-large' type='text'>{result && result[0]['item'].name}</td>
+            <td>{getNutritionalValue('P')}</td>
+            <td>{getNutritionalValue('I')}</td> 
+            <td>{getNutritionalValue('Fe')}</td>
+            <td>{getNutritionalValue('Ca')}</td>
+            <td>{getNutritionalValue('K')}</td>
+            <td>{getNutritionalValue('Cu')}</td> 
+            <td>{getNutritionalValue('Mg')}</td>
+            <td>{getNutritionalValue('Se')}</td>
+            <td>{getNutritionalValue('Zn')}</td>
         </tr>
     );
 }
