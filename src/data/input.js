a7b3ea46aa561b9247fe49076d2348d83f041e5b
milliampere <millla@hotmail.com>
2018-02-27 13:45:33 +0100

    Moved data files to folder.

---
+++ src/data/input.js
@@ -0,0 +1,10 @@
+export default [{ amount: "800", type: "g", name: "potatis", group: "potatis" },
+    { amount: "200", type: "g", name: "halloumiost", group: "ost" },
+    { amount: "2", name: "tomater", group: "grönsaker" },
+    { amount: "4", name: "champinjonburgare", group: "färdigmat" },
+    { amount: "4", name: "hamburgerbröd av surdeg", group: "bröd" },
+    { amount: "2", type: "dl", name: "lätt crème fraiche med tacosmak", group: "crème fraiche" },
+    { amount: "2", type: "tsk", name: "olja", group: "olja" },
+    { name: "salt", group: "kryddor"  },
+    { name: "peppar", group: "kryddor" }
+]
