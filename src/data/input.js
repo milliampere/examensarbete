ed98d973639639e4ba5e055ec3b00ae652caeaf4
milliampere <millla@hotmail.com>
2018-03-01 21:44:44 +0100

    Input examples to test search function.

--- src/data/input.js
+++ src/data/input.js
@@ -8,3 +8,30 @@ export default [{ amount: "800", type: "g", name: "potatis"},
     { name: "salt"},
     { name: "peppar"}
 ]
+
+// testar olika recept
+
+/* export default [
+    {name: "majskorn (à 340 g)"},
+    {name: "kokta gröna linser (à 380 g)" },
+    {name: "krossade tomater med vitlök" },
+    {name: "passerade tomater" },
+    {name: "soltorkad tomatcrème" },
+    {name: "lasagneplattor" },
+    {name: "lätt crème fraiche" },
+    {name: "salad" }
+] */
+
+/* export default [
+    {name: "gul lök"},
+    {name: "morötter"},
+    {name: "rapsolja"},
+    {name: "currypulver"},
+    {name: "mjölk"},
+    {name: "grönsaksbuljongtärning"},
+    {name: "majsstärkelse"},
+    {name: "stekt & skivad minutfilé av kyckling à 200 g (gärna classic)"},
+    {name: "fryst, tärnad mango"},
+    {name: "kokosflingor"},
+    {name: "ris"}
+] */
