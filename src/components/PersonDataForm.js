b4cc47edf40e0a8f13899fc6999c76c305295558
milliampere <millla@hotmail.com>
2018-03-11 19:35:20 +0100

    Some validation.

--- src/components/PersonDataForm.js
+++ src/components/PersonDataForm.js
@@ -9,7 +9,12 @@ export default class PersonDataForm extends Component {
 		lengthCm: '',
 		weightKg: '',
 		ageYear: '',
-		PAL: '',    // physical activity level
+        PAL: '',    // physical activity level
+        formErrors: {lengthCm: '', weightKg: '', ageYear: ''},
+        lengthCmValid: false,
+        weightKgValid: false,
+        ageYearValid: false,
+        formValid: false
     }
 
     componentDidMount() {
@@ -17,8 +22,44 @@ export default class PersonDataForm extends Component {
     }
 
     onChange = (event) => {
-		this.setState({[event.target.name] : event.target.value})
+        let name = event.target.name;
+        let value = event.target.value;
+        this.setState({[name] : value}, 
+            () => { this.validateField(name,value) }               // validation callback
+        )
     }
+
+    validateField(inputName, value) {
+        let fieldValidationErrors = this.state.formErrors;
+        let {ageYearValid, lengthCmValid, weightKgValid} = this.state;
+      
+        switch(inputName) {
+            case 'ageYear':
+                ageYearValid = value.match(/^([1][8-9]|[2-9][0-9]|[1][0-1][0-9])$/i);   //18-119
+                fieldValidationErrors.ageYear = ageYearValid ? '' : ' Ogiltig ålder (>18 år)';
+                break;
+            case 'lengthCm':
+                lengthCmValid = value.match(/^([1-2][0-9][0-9])$/i);    // 100-299
+                fieldValidationErrors.lengthCm = lengthCmValid ? '': ' Ogiltig längd';
+                break;
+            case 'weightKg':
+                weightKgValid = value.match(/^([4-9][0-9]|[1-2][0-9][0-9])$/i);     // 30-299
+                fieldValidationErrors.weightKg = weightKgValid ? '': ' Ogiltig vikt';
+                break;
+            default:
+                break;
+        }
+        this.setState({formErrors: fieldValidationErrors,
+            ageYearValid: ageYearValid,
+            lengthCmValid: lengthCmValid,
+            weightKgValid: weightKgValid
+            }, this.validateForm);
+      }
+      
+      validateForm() {
+        this.setState({formValid: this.state.ageYearValid && this.state.lengthCmValid && this.state.weightKgValid});
+      }
+      
 
 
     render() {
@@ -33,16 +74,19 @@ export default class PersonDataForm extends Component {
                 <div>
                     <p>Ålder:</p>
                     <input type="text" name="ageYear" onChange={this.onChange} value={this.state.ageYear} placeholder="Ålder"></input>
+                    {this.state.formErrors.ageYear}
                 </div>
                 <br/>
                 <div>
                     <p>Längd i cm:</p>
                     <input type="text" name="lengthCm" onChange={this.onChange} value={this.state.lengthCm} placeholder="Längd i cm"></input>
+                    {this.state.formErrors.lengthCm}
                 </div>
                 <br/>
                 <div>
                     <p>Vikt i kg:</p>
                     <input type="text" name="weightKg" onChange={this.onChange} value={this.state.weightKg} placeholder="Vikt i kg"></input>
+                    {this.state.formErrors.weightKg}
                 </div>
                 <br/>
                 <p>Fysisk aktivitet:</p>
@@ -54,7 +98,9 @@ export default class PersonDataForm extends Component {
                     <option name="PAL" value="2.2">Tungt kroppsarbete eller mycket hög fritidsaktivitet</option>
                 </select>
                 <br/>
-                <input className="btn btn-primary" type="submit" value="Spara" />
+                {this.state.formValid ? <input className="btn btn-primary" type="submit" value="Spara" />
+                : <input className="btn btn-primary" type="submit" value="Spara" disabled /> }
+                
             </form>
             )
     }
