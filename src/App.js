3e82756f3d8e13689c286783fae363f12d9af469
milliampere <millla@hotmail.com>
2018-03-13 10:48:24 +0100

    Plugin message if not correct recipe website.

--- src/App.js
+++ src/App.js
@@ -139,52 +139,66 @@ class App extends Component {
 			PAL: this.state.PAL,
 		}
 
+		
+/* 		if(!this.state.rawInputArray){
+			return (
+				<div className="App">
+					<div className="use-extension-info">
+						<p>Besök någon av följande sidor för att använda pluginet</p>
+						<p>ica.se</p>
+						<p>coop.se</p>
+						<p>koket.se</p>
+					</div>
+				</div>
+			)
+		}else { */
 
-		return (
-			<div className="App">
-				<div className="header-part">
-					{!this.state.showPersonDataForm &&
-						<Navigation
-							activeTab={this.state.activeTab}
+			return (
+				<div className="App">
+					<div className="header-part">
+						{!this.state.showPersonDataForm &&
+							<Navigation
+								activeTab={this.state.activeTab}
+								allNutrients={propsdataallNutrients}
+								onClick={this.handleButtonClick}
+							/>
+						}
+						{!this.state.showPersonDataForm &&
+							<div>
+								<h1>Näringsberäknaren</h1>
+								<div className="portions-div">	
+									<p className="portions-text" >Antal portioner:</p>
+									<select value={this.state.portions} selected={this.state.portions} onChange={this.handlePortionChange}>
+										{selectOptions}
+									</select>
+								</div>
+							</div>
+						}
+					</div>
+					{!this.props.data.loading && !this.state.showPersonDataForm &&
+						<Content
+							//rawInputArray={this.state.rawInputArray}
+							rawInputArray={rawInputArray}
+							portions={this.state.portions}
 							allNutrients={propsdataallNutrients}
-							onClick={this.handleButtonClick}
+							activeTab={this.state.activeTab}
+							options={options}
+							personalGroup={this.state.personalGroup}
+							allFoodsData={this.props.data.allFoods}
 						/>
 					}
-					{!this.state.showPersonDataForm &&
-						<div>
-							<h1>Näringsberäknaren</h1>
-							<div className="portions-div">	
-								<p className="portions-text" >Antal portioner:</p>
-								<select value={this.state.portions} selected={this.state.portions} onChange={this.handlePortionChange}>
-									{selectOptions}
-								</select>
-							</div>
-						</div>
-					}
-				</div>
-				{!this.props.data.loading && !this.state.showPersonDataForm &&
-					<Content
-						//rawInputArray={this.state.rawInputArray}
-						rawInputArray={rawInputArray}
-						portions={this.state.portions}
-						allNutrients={propsdataallNutrients}
-						activeTab={this.state.activeTab}
+					<PersonDataForm
+						onClick={this.changePersonData}
+						onSubmit={this.onSubmit}
 						options={options}
+						close={this.closePersonData}
+						show={this.state.showPersonDataForm}
 						personalGroup={this.state.personalGroup}
-						allFoodsData={this.props.data.allFoods}
 					/>
-				}
-				<PersonDataForm
-					onClick={this.changePersonData}
-					onSubmit={this.onSubmit}
-					options={options}
-					close={this.closePersonData}
-					show={this.state.showPersonDataForm}
-					personalGroup={this.state.personalGroup}
-				/>
-				<Credits />
-			</div>
-		);
+					<Credits />
+				</div>
+			);
+		/* } */
 	}
 }
 
